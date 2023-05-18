6a14c53434782a8d7a2fbb97a94585d36e53bfed
Sonali <102951511+sonaliidas@users.noreply.github.com>
2023-05-18 21:53:02 +0530

    Update mini_voice_assistant.py

--- mini_voice_assistant.py
+++ mini_voice_assistant.py
@@ -9,8 +9,8 @@ listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
-engine.say('Hi, I am tara')
-engine.say('What can i do for you')
+engine.say('Hi, I am Pipi')
+engine.say('What can I do for you')
 engine.runAndWait()
 def talk(text):
     engine.say(text)
@@ -22,14 +22,14 @@ def take_command():
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
             command = command.lower()
-            if 'tara' in command:
-                command = command.replace('tara', '')
+            if 'pipi' in command:
+                command = command.replace('pipi', '')
                 print(command)
     except:
         pass
     return command
 
-def run_tara():
+def run_pipi():
     command = take_command()
     print(command)
     if 'play' in command:
@@ -41,12 +41,20 @@ def run_tara():
         print(time)
         talk('Current time is ' + time)
     elif 'tell me about' in command:
-        serchresult = command.replace('tell me about', '')
-        info = wikipedia.summary(serchresult, 2)
+        searchresult = command.replace('tell me about', '')
+        info = wikipedia.summary(searchresult, 2)
         print(info)
         talk(info)
-    elif 'love' in command:
-        talk('i love you sonali')
+    elif 'do you love me' in command:
+        talk('Everyone loves you and so do i')
+    elif 'i love you' in command:
+        talk('I love you too')
+    elif 'who do you love the most' in command:
+        talk('Myself')
+    elif 'who is sonali' in command:
+        talk('She the one who created me and I adore her for that you can know more about her from her github dashboard or her social profiles')
+    elif 'do you love sonali' in command:
+        talk('Ofcourse I love her')
     elif 'make me laugh' in command:
         talk(pyjokes.get_joke())
     elif 'sad' in command:
@@ -54,13 +62,19 @@ def run_tara():
         talk(pyjokes.get_joke())
     elif 'thank you' in command:
         talk("You're welcome")
+    elif 'i want to sleep' in command:
+        talk('Ya you can take a power nap')
+    elif 'you sleep' in command:
+        talk('If you want me to sleep just say sleep well')
+    elif 'sleep well' in command:
+        talk('Thank you Bye')
     elif 'bye' in command:
-        talk('Bye')
+        talk('Yup Bye')
     else:
         talk('Please say that again')
 try:
     while True:
-        run_tara()
+        run_pipi()
 except:
     print('See you again. Take care.')
     talk('See you again. Take care.')
